81d0d39b1a3d8c754ada723c7b11a88f241dd51b
sartek430 <macogneur430@gmail.comm>
2023-10-12 12:26:54 +0200

    sideBar fonctionnel avec home/profile/settings

---
+++ frontend/src/components/SideBar.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import {
+  Box,
+  Flex,
+  HStack,
+  List,
+  ListItem,
+  Link,
+  Text,
+} from "@chakra-ui/react";
+import { BiHomeAlt2 } from "react-icons/bi";
+import { LuSettings } from "react-icons/lu";
+import { IoPersonOutline } from "react-icons/io5";
+
+const SideBar = () => {
+  const [collapse, setCollapse] = React.useState(false);
+
+  const items = [
+    {
+      label: "Home",
+      path: "freelanceHelper/home",
+      icon: BiHomeAlt2,
+    },
+    {
+      label: "Profile",
+      path: "freelanceHelper/profile",
+      icon: IoPersonOutline,
+    },
+    {
+      label: "Settings",
+      path: "freelanceHelper/settings",
+      icon: LuSettings,
+    },
+  ];
+
+  return (
+    <HStack w="full" h="100vh">
+      <Flex
+        as="aside"
+        w="full"
+        h="full"
+        maxW={collapse ? "96px" : "250px"}
+        bg="gray.900"
+        alignItems="center"
+        flexDirection="column"
+        padding={4}
+        justifyContent={"space-between"}
+        onMouseEnter={() => setCollapse(false)}
+        onMouseLeave={() => setCollapse(true)}
+      >
+        <Box w="full">
+          <List>
+            {items.map((item, index) => (
+              <ListItem key={index}>
+                <NavItem
+                  item={item}
+                  isActive={index === 0}
+                  collapse={collapse}
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Flex>
+    </HStack>
+  );
+};
+
+export default SideBar;
+
+const NavItem = ({ item, isActive, collapse }) => {
+  const { icon, label, path } = item;
+
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      p={4}
+      borderRadius={8}
+      justifyContent="center"
+      bg={isActive ? "gray.800" : ""}
+    >
+      <Link
+        href={path}
+        as={Link}
+        display="flex"
+        gap={10}
+        alignItems="center"
+        fontWeight="medium"
+        w="full"
+        color={isActive ? "brand.500" : "gray.200"}
+        _hover={{
+          color: "brand.500",
+          textDecoration: "none",
+          fontWeight: "bold",
+          fontSize: 18,
+        }}
+        justifyContent={collapse ? "center" : ""}
+      >
+        <List as={icon} fontSize={24} m={1} />
+        {!collapse && <Text>{label}</Text>}
+      </Link>
+    </Box>
+  );
+};
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Box } from "@chakra-ui/react";
+import SideBar from "../components/SideBar";
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <Box>
+      <SideBar />
+    </Box>
   );
 };
 
